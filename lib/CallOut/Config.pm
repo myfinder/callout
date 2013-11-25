9c59ba94e38a7e907b0a2ce5c6158e198e8ff68a
Tatsuro Hisamori <medianetworks@gmail.com>
2013-11-25 16:03:32 +0900

    added config module

---
+++ lib/CallOut/Config.pm
@@ -0,0 +1,19 @@
+package CallOut::Config;
+use strict;
+use warnings;
+use File::Basename qw/dirname/;
+use Exporter qw/import/;
+our @EXPORT_OK qw/config/;
+
+{
+    my $config;
+    sub config() {
+        $config //= do {
+            my $script_dir = $ENV{CallOut_CONFIG_PATH} || dirname $0;
+            do "$script_dir/config.pl";
+        }
+    }
+}
+
+1;
+__END__
