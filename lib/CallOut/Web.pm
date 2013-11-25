30280dbfb7e70e98c4d9da2aa48f73226f10306e
Tatsuro Hisamori <medianetworks@gmail.com>
2013-11-25 17:36:52 +0900

    適当だけどひとまずpostまで確認済

--- lib/CallOut/Web.pm
+++ lib/CallOut/Web.pm
@@ -22,9 +22,22 @@ get '/members' => sub {
     $c->render_json($list);
 };
 
-#post '/message' => sub {
-#
-#}
+post '/message' => sub {
+    my ($self, $c) = @_;
+
+    my $ua = LWP::UserAgent->new;
+    my $url = config->{'notification_url'} . config->{'auth_token'};
+    my $req = HTTP::Request->new(POST => $url);
+    my $mention_name = $c->req->param('mention_name');
+    #my $content = encode_json {  message => "ほげ さんに来客です" };
+    my $content = "room_id=352440&from=Alerts&message=dareka+kita";
+    #$req->header('Content-Type' => 'application/json');
+    $req->header('Content-Type' => 'application/x-www-form-urlencoded');
+    $req->content($content);
+    my $res = $ua->request($req);
+
+    warn $res;
+};
 
 1;
 __END__
