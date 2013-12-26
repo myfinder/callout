89b71cd975675fbce0b01056ca394cf14cf794ab
hiroyukim <hiroyukim@fout.jp>
2013-12-26 13:25:35 +0900

    admin画面を追加

--- lib/CallOut/Web.pm
+++ lib/CallOut/Web.pm
@@ -6,49 +6,101 @@ use utf8;
 use Kossy;
 use JSON::XS;
 use CallOut::Config qw/config/;
-use CallOut::Api::HipChat;
+use CallOut::Container 'container';
+use CallOut::Cmd::Member;
 use JSON;
 use LWP::UserAgent;
 
-my $hipchat_client = CallOut::Api::HipChat->new( auth_token => config->{auth_token} );
+get '/admin/member/' => sub {
+    my ($self, $c) = @_;
+
+    $c->render('/admin/member/index.tx', { members => container('db')->select_all("SELECT * FROM member") // [] });
+};
+
+get '/admin/syllabary/' => sub {
+    my ($self, $c) = @_;
+    my $syllabary = container('db')->select_all("
+        SELECT 
+            syllabary.*, member.name AS member_name
+        FROM syllabary 
+            JOIN syllabary_member ON syllabary_member.syllabary_id = syllabary.id    
+            JOIN member           ON syllabary_member.member_id    = member.id
+            ORDER BY syllabary.name
+    "); 
+    
+    $c->render('/admin/syllabary/index.tx', { 
+        syllabary => $syllabary,
+    });
+};
+
+
+get '/admin/member/show' => sub {
+    my ($self, $c) = @_;
+    my $member = container('db')->select_row("
+        SELECT member.* ,syllabary.name AS syllabary_name, syllabary.id AS syllabary_id 
+        FROM member 
+            JOIN syllabary_member ON member.id = syllabary_member.member_id 
+            JOIN syllabary        ON syllabary_member.syllabary_id = syllabary.id    
+        WHERE member.id = ?", $c->req->param('member_id') //'' ); 
+
+    $c->render('/admin/member/show.tx', { 
+        member => $member 
+    });
+};
+
+post '/admin/member/syllabary/update' => sub {
+    my ($self, $c) = @_;
+
+    my $member_id      = $c->req->param('member_id') or die 'require member_id';
+    my $syllabary_id   = $c->req->param('syllabary_id') or die 'require syllabary_id';
+    my $syllabary_name = $c->req->param('syllabary_name') or die 'require syllabary_name';
+ 
+    my $syllabary = container('db')->select_row("SELECT * FROM syllabary WHERE name = ?", $syllabary_name);
+
+    if( $syllabary ) {
+        container('db')->do('UPDATE syllabary_member SET syllabary_id = ? WHERE member_id = ? ',{}, $syllabary->{id},$member_id);
+    }
+    else {
+        container('db')->begin_work();
+
+        eval{
+            container('db')->do("INSERT INTO syllabary (name) VALUES(?)",{}, $syllabary_name,);
+            my $new_syllabary = container('db')->select_row("SELECT * FROM syllabary WHERE name = ?", $syllabary_name);
+            container('db')->do('UPDATE syllabary_member SET syllabary_id = ? WHERE member_id = ? ',{}, $new_syllabary->{id},$member_id);
+            container('db')->commit();
+        };
+        if($@) {
+            container('db')->rollback();
+            die $@;
+        }
+    }
+
+    container('db')->do("DELETE FROM syllabary WHERE id IN (SELECT id FROM syllabary WHERE id NOT IN (SELECT DISTINCT(syllabary_id) FROM syllabary_member))"); 
+
+    $c->redirect('/admin/member/show?member_id=' . $member_id);
+};
 
 get '/' => sub {
     my ($self, $c) = @_;
 
-    my $users = []; 
-    eval {
-        $users = $hipchat_client->get_all_users();
-    };
-
-    $c->render('index.tx', { users => $users->{'items'} });
+    $c->render('index.tx', { users => container('db')->select_all("SELECT * FROM member") });
 };
 
 get '/members' => sub {
     my ($self, $c) = @_;
-
-    my $users; 
-    eval {
-        $users = $hipchat_client->get_all_users();
-    };
-    if($@) {
-        return $c->render_json({ result => 0 });
-    }
-
-    $c->render_json($users);
+    $c->render_json(
+        container('db')->select_all("SELECT * FROM member")
+    );
 };
 
 get '/view_user' => sub {
     my ($self, $c) = @_;
 
-    my $view_user; 
-    eval {
-        $view_user = $hipchat_client->view_user({ user_id => $c->req->param('user_id') });
-    };
+    my $view_user = container('db')->select_row("SELECT * FROM member WHERE id = ?",$c->req->param('user_id'));
 
-    if($@) {
+    if($view_user) {
         $c->render_json({ result => 0 });
     }
-
 
     if( config->{view_user} && ref(config->{view_user}->{permit_params}) eq 'ARRAY' ) {
         $view_user = +{ map { $_ => $view_user->{$_} } @{config->{view_user}->{permit_params}} }; 
@@ -62,7 +114,21 @@ post '/message' => sub {
     my ($self,$c) = @_;
 
     eval {
-        $hipchat_client->send_room_notification({ room => config->{room}, message => $c->req->param('message') //'' });
+        container('hipchat')->send_room_notification({ room => config->{room}, message => $c->req->param('message') //'' });
+    };
+
+    if($@) {
+        return $c->render('message.tx', {result => 0});
+    }
+
+    $c->render('message.tx', {result => 1});
+};
+
+post '/update_member' => sub {
+    my ($self,$c) = @_;
+
+    eval {
+        CallOut::Cmd::Member->update;
     };
 
     if($@) {
