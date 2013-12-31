ec816e39e7f7fb621a0d1bb188421aee76beff6d
Akiko Asami <gaooh@gaooh.local>
2013-12-31 21:23:50 +0900

    デザイン一式

---
+++ public/js/main.js
@@ -0,0 +1,55 @@
+$(function(){
+    var characters = ["blowfish", "celebration", "dog4", "sheep1", "silhouette2"];
+
+    $(".js-syllabary").click(function () {
+        var index = $(this).data("index");
+
+        $("ul#member-list li").each(function (i) {
+            if($(this).data("syllabary-index") === index) {
+                $(this).show();
+            }
+            $("#office-vistiors-content").hide();
+            $("#staff-vistiors-content").hide();
+            $(".js-return-view-list").show();
+        });
+    });
+
+    $('.js-return-view-list').click(function () {
+        $("#office-vistiors-content").show();
+        $("#staff-vistiors-content").show();
+        $(".js-return-view-list").hide();
+
+        $("ul#member-list li").each(function (i) {
+            $(this).hide();
+        });
+    });
+
+    $('.js-member-box').click(function() {
+        var params = $(this).find("form").serialize();
+        var randnum = Math.floor( Math.random() * characters.length );
+
+        $("#character").removeClass();
+        $("#character").addClass("character");
+
+        $.post( "/message", params,
+            function() {
+                $("#response-message").show();
+            })
+            .done(function() {
+                $("#character").addClass("flaticon-" + characters[randnum]);
+                $("#character").effect("bounce", {}, 4000);
+                setTimeout(function(){
+                    $("#response-message").toggle("fade", {}, 1000);
+                },5000);
+            })
+            .fail(function() {
+                $("#response-error-message").show();
+                setTimeout(function(){
+                    $("#response-error-message").toggle("fade", {}, 1000);
+                },5000);
+            })
+            .always(function() {
+            });
+
+    });
+});
