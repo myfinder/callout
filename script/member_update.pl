79b7b6687ff37dc8094caa84cd6d654fcc030774
Tatsuro Hisamori <medianetworks@gmail.com>
2014-01-06 15:14:10 +0900

    初期化アップデートスクリプト修正と、sqlite3のファイルをignoreする

--- script/member_update.pl
+++ script/member_update.pl
@@ -1,5 +1,9 @@
+#!/usr/bin/env perl
 use strict;
 use warnings;
+use FindBin;
+use lib "$FindBin::Bin/../extlib/lib/perl5";
+use lib "$FindBin::Bin/../lib";
 use CallOut::Cmd::Member;
 
 CallOut::Cmd::Member->update;
